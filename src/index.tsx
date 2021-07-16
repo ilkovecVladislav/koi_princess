cb034a3b1b9cc7c3677a36332dee8d0e78b1287c
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2021-07-16 16:55:01 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,8 @@ import App from './App';
 Sentry.init({
   dsn: 'https://82f7054f610a414a851fefeff7b3f78b@o502735.ingest.sentry.io/5868302',
   integrations: [new Integrations.BrowserTracing()],
-  release: process.env.REACT_APP_SENTRY_RELEASE,
-  autoSessionTracking: true,
+  // release: process.env.REACT_APP_SENTRY_RELEASE,
+  // autoSessionTracking: true,
   tracesSampleRate: 1.0,
 });
 
