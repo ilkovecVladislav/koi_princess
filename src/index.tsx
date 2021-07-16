bd4e11a8b7f6a0f91cbd0fae5feab721005373e6
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2021-07-16 17:40:12 +0300

    netlify fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 
 import './index.css';
 import App from './App';
-
-Sentry.init({
-  dsn: 'https://82f7054f610a414a851fefeff7b3f78b@o502735.ingest.sentry.io/5868302',
-  integrations: [new Integrations.BrowserTracing()],
-  autoSessionTracking: true,
-  release: '0.1.0',
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
   <React.StrictMode>
