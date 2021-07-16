c7cb5dbe2a4b44941bef6f94ed76584023fe5ab9
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2021-07-16 16:50:27 +0300

    sentry fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 import './index.css';
 import App from './App';
 
-if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SENTRY_RELEASE) {
-  Sentry.init({
-    dsn: 'https://82f7054f610a414a851fefeff7b3f78b@o502735.ingest.sentry.io/5868302',
-    release: process.env.REACT_APP_SENTRY_RELEASE,
-  });
-}
+Sentry.init({
+  dsn: 'https://82f7054f610a414a851fefeff7b3f78b@o502735.ingest.sentry.io/5868302',
+  integrations: [new Integrations.BrowserTracing()],
+  release: process.env.REACT_APP_SENTRY_RELEASE,
+  autoSessionTracking: true,
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
